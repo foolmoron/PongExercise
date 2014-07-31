1aa4f15d40b4185f5bbd053ed390b21b1eb532ec
Momin Khan <foolmoron@gmail.com>
2014-07-31 10:47:46 -0400

    Added W/S controls

--- Assets/PlayerPaddle.cs
+++ Assets/PlayerPaddle.cs
@@ -12,14 +12,14 @@ public class PlayerPaddle : MonoBehaviour {
 	
 	void Update() {
 		var newPosition = transform.position;
-		if (Input.GetKey(KeyCode.UpArrow)) {
+		if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
 			newPosition.y += MovementSpeed * Time.deltaTime;
 			if (newPosition.y > MovementRange/2) {
 				newPosition.y = MovementRange/2;
 			}
 			transform.position = newPosition;
 			Instructions.SetActive(false);
-		} else if (Input.GetKey(KeyCode.DownArrow)) {
+		} else if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
 			newPosition.y -= MovementSpeed * Time.deltaTime;
 			if (newPosition.y < -MovementRange/2) {
 				newPosition.y = -MovementRange/2;
